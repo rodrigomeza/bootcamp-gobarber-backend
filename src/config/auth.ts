f6dd3a5120fc5d8acf7292c0aea91bdeb7bff55b
Rodrigo Meza <jremeza@gmail.com>
2020-04-19 16:16:37 -0300

    incluindo imagens no perfil do usuario

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '237791b3ba8b2ae2cb9c386d5fc8e178',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    // Para deletar o password do retorno da função, mas não deleta do banco
+    delete user.password;
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, (request, response) => {
+  return response.json({ ok: true });
+});
+
+export default usersRouter;
